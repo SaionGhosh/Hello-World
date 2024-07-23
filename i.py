6f592b2e8661d4a5282270330b56ef9d727b836d
SaionGhosh <saion331ghosh@gmail.com>
2024-07-23 16:34:13 +0530

    Initializing

---
+++ i.py
@@ -0,0 +1,107 @@
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m' # orange on some systems
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+LIGHT_GRAY = '\033[37m'
+DARK_GRAY = '\033[90m'
+BRIGHT_RED = '\033[91m'
+BRIGHT_GREEN = '\033[92m'
+BRIGHT_YELLOW = '\033[93m'
+BRIGHT_BLUE = '\033[94m'
+BRIGHT_MAGENTA = '\033[95m'
+BRIGHT_CYAN = '\033[96m'
+WHITE = '\033[97m'
+
+import time as ti
+alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",]
+for i in alphabet:
+    if i == "h":
+        print("h")
+        break
+    else:
+         print(i)
+         ti.sleep(55/1000)
+######################
+for j in alphabet:
+    if j == "e":
+        print("h"+"e")
+        break
+    else:
+         print("h"+j)
+         ti.sleep(55/1000)
+######################
+for k in alphabet:
+    if k == "l":
+        print("h"+"e"+"l")
+        break
+    else:
+         print("h"+"e"+k)
+         ti.sleep(55/1000)
+######################
+for l in alphabet:
+    if l == "l":
+        print("h"+"e"+"l"+"l")
+        break
+    else:
+         print("h"+"e"+"l"+l)
+         ti.sleep(55/1000)
+######################
+for o in alphabet:
+    if o == "o":
+        print("h"+"e"+"l"+"l"+"o")
+        break
+    else:
+         print("h"+"e"+"l"+"l"+o)
+         ti.sleep(55/1000)
+######################
+for q in alphabet:
+    if q == " ":
+        print("h"+"e"+"l"+"l"+"o"+" ")
+        ti.sleep(55/1000)
+        break
+    else:
+         print("h"+"e"+"l"+"l"+"o"+q)
+         ti.sleep(55/1000)
+######################
+for w in alphabet:
+    if w == "w":
+        print("h"+"e"+"l"+"l"+"o"+" "+"w")
+        break
+    else:
+         print("h"+"e"+"l"+"l"+"o"+" "+w)
+         ti.sleep(55/1000)
+######################
+for e in alphabet:
+    if e == "o":
+        print("h"+"e"+"l"+"l"+"o"+" "+"w"+"o")
+        break
+    else:
+         print("h"+"e"+"l"+"l"+"o"+" "+"w"+e)
+         ti.sleep(55/1000)
+######################
+for f in alphabet:
+    if f == "r":
+        print("h"+"e"+"l"+"l"+"o"+" "+"w"+"o"+"r")
+        break
+    else:
+         print("h"+"e"+"l"+"l"+"o"+" "+"w"+f)
+         ti.sleep(55/1000)
+######################
+for p in alphabet:
+    if p == "l":
+        print("h"+"e"+"l"+"l"+"o"+" "+"w"+"o"+"r"+"l")
+        break
+    else:
+         print("h"+"e"+"l"+"l"+"o"+" "+"w"+"o"+p)
+         ti.sleep(55/1000)
+######################
+for d in alphabet:
+    if d == "d":
+        print("h"+"e"+"l"+"l"+"o"+" "+"w"+"o"+"r"+"l"+"d")
+        break
+    else:
+         print("h"+"e"+"l"+"l"+"o"+" "+"w"+"o"+"l"+d)
+         ti.sleep(55/1000)
